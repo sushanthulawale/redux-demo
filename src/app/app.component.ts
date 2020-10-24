508b43161b7a10696baeddca22c636568ce2470f
Sushant Hulawale <sushanthulawale@gmail.com>
2020-10-24 18:38:22 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { Observable } from 'rxjs';
+import { IAppState } from './store';
 import { Component } from '@angular/core';
+import { NgRedux, select } from 'ng2-redux';
+import { INCREMENT } from './action';
+import { Map} from 'immutable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-demo';
+  //counter = 0;
+  //@select() counter: Observable<number>;
+  @select(s => s.get('counter')) counter;
+
+  constructor(private ngRedux: NgRedux<Map<string, any>>){
+    // var subscription = ngRedux.subscribe(() => {
+    // var store = ngRedux.getState();
+    // this.counter = store.counter;
+    //  });
+  }
+  increment(){
+    //this.counter++;
+    this.ngRedux.dispatch({type: INCREMENT });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, isDevMode } from '@angular/core';
+import { NgRedux, NgReduxModule, DevToolsExtension} from 'ng2-redux';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INITIAL_STATE} from './store';
+import { fromJS, Map} from 'immutable';
+
+
+// import { AppComponent } from './app.component';
+// import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(ngRedux: NgRedux<IAppState>,
+      devtools: DevToolsExtension){
+        var enhancer= isDevMode() ?  [devtools.enhancer()] : [];
+        //ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE));
+        ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancer);
+     }
+}
